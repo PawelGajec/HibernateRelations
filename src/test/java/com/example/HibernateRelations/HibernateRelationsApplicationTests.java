ab0678cb1f2f78ddacc0e8a30dae85fd257082d2
PawelGajec <gajec.pawel@gmail.com>
2022-06-12 10:30:56 +0200

    Initial commit

---
+++ src/test/java/com/example/HibernateRelations/HibernateRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HibernateRelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
